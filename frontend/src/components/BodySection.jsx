a4a45d5432817e4021caf51603a0f92376f530c1
Ofentse <ofentsemakhutja@icloud.com>
2025-09-04 19:15:27 +0200

    Add education and certifications sections to profile
    Introduced new 'Education' and 'Certifications' sections in BodySection.jsx to provide more detailed background information. Updated NavBar.jsx to include navigation links to these sections and improved styling. Enhanced index.css with new styles for headings and bordered content blocks.

--- frontend/src/components/BodySection.jsx
+++ frontend/src/components/BodySection.jsx
@@ -5,17 +5,14 @@ export default function BodySection() {
     <>
       <section className="pt-30 endless-river min-h-[100vh] space-y-3 flex items-center flex-col">
         <img src={image} className="rounded-full size-60 mb-8 " />
-        <div className="font-bold text-zinc-100 text-xl flex items-center flex-col ">
+        <div className="font-bold text-xl flex items-center flex-col ">
           <p>Hey😁👋, I'm Ofentse Makhutja</p>
-          <p className="text-[#43cea2]">Web Developer</p>
+          <p className="text-[#43cea2]">Programmer</p>
         </div>
       </section>
-      <section
-        id="about-me"
-        className="endless-river text-zinc-100 items-center space-y-8"
-      >
+      <section id="about-me" className="endless-river items-center space-y-8">
         <div className="flex flex-col px-10">
-          <h1 className="font-bold text-2xl pb-4">About Me</h1>
+          <h1>About Me</h1>
           <div className=" p-6 rounded-2xl border-2 max-w-4xl m-auto">
             <p>
               I am a dedicated and enthusiastic programmer seeking a practical
@@ -79,6 +76,71 @@ export default function BodySection() {
               <li>Good Communication</li>
               <li>Teamwork</li>
             </ul>
+          </div>
+        </div>
+      </section>
+      <section id="education" className=" endless-river p-10">
+        <h1>Education</h1>
+        <div className="flex flex-wrap flex-row gap-8 items-center justify-center mt-4">
+          <div className="borderedDivs">
+            <h3>Diploma in Computer Science</h3>
+            <p>Tshwane University of Technology</p>
+            <p>2022-2025</p>
+            <p>Currently Studying</p>
+          </div>
+          <div className="borderedDivs">
+            <h3>National Senior Certificate</h3>
+            <p>Allanridge Secondary School</p>
+            <p>Math, Physical Sci, Life Sci, Geo, Eng, Afr</p>
+            <p>2017 - 2021</p>
+          </div>
+          <div className="borderedDivs">
+            <h3>Business Support Systems Level 5</h3>
+            <p>Dynamic DNA</p>
+            <p>Learnership for work readiness program</p>
+            <p>2024 - 2025</p>
+          </div>
+        </div>
+      </section>
+      <section id="certifications" className="endless-river px-10">
+        <h1>Certifications</h1>
+        <div className="flex justify-center items-center pt-4">
+          <div className="borderedDivs mb-10 w-5xl flex gap-6">
+            <div>
+              <h3>Microsoft</h3>
+              <div className="flex flex-col ml-4">
+                <a
+                  href="https://learn.microsoft.com/api/credentials/share/en-us/OfentseMakhutja-8865/680FECAF141D66F9?sharingId"
+                  target="_blank"
+                >
+                  - AZ-900 <i class="ri-external-link-line"></i>
+                </a>
+                <a
+                  href=" https://learn.microsoft.com/api/credentials/share/en-us/OfentseMakhutja-8865/43E1BBFB203FF580?sharingId "
+                  target="_blank"
+                >
+                  - DP-900 <i class="ri-external-link-line"></i>
+                </a>
+              </div>
+            </div>
+            <div>
+              <h3>freeCodeCamp</h3>
+              <div className="flex flex-col ml-4">
+                <a
+                  href="https://www.freecodecamp.org/certification/ofentsemakhutja/responsive-web-design"
+                  target="_blank"
+                >
+                  - Responsive Web Design <i class="ri-external-link-line"></i>
+                </a>
+                <a
+                  href="https://www.freecodecamp.org/certification/ofentsemakhutja/javascript-algorithms-and-data-structures-v8"
+                  target="_blank"
+                >
+                  - JavaScript Algoriths and Data Structuse{" "}
+                  <i class="ri-external-link-line"></i>
+                </a>
+              </div>
+            </div>
           </div>
         </div>
       </section>
--- frontend/src/components/NavBar.jsx
+++ frontend/src/components/NavBar.jsx
@@ -1,15 +1,25 @@
 export default function NavBar() {
   return (
     <>
-      <div className="endless-river flex justify-between p-4 min-w-screen fixed shadow-2xl text-zinc-100">
-        <h1 className="font-bold text-2xl">Ofentse.</h1>
-        <div className="flex gap-4 pr-2 font-medium">
-          <a href="#about-me">About Me</a>
-          <a href="#">Education</a>
-          <a href="#">Certication</a>
-          <a href="#">Projects</a>
-          <a href="#">Contacts</a>
-        </div>
+      <div className="endless-river flex justify-between p-4 min-w-screen fixed shadow-2xl text-zinc-100 items-center">
+        <h2 className="font-bold text-2xl">Ofentse.</h2>
+        <nav className="flex gap-6 pr-2 font-medium">
+          <a href="#about-me" className="hover:border-b-2">
+            About Me
+          </a>
+          <a href="#education" className="hover:border-b-2">
+            Education
+          </a>
+          <a href="#certifications" className="hover:border-b-2">
+            Certications
+          </a>
+          <a href="#" className="hover:border-b-2">
+            Projects
+          </a>
+          <a href="#" className="hover:border-b-2">
+            Contacts
+          </a>
+        </nav>
       </div>
     </>
   );
