b0c27421c7f8f880e837cdd9ce1d5811069f2261
Ofentse <ofentsemakhutja@icloud.com>
2025-09-11 12:32:29 +0200

    Refactor NavBar shadow and open state styles
    Simplifies the NavBar's shadow styling by removing conditional shadow classes from the <nav> element and moving them to the <ul> element based on the open state. Also fixes a typo in the 'shadow-none' class and adjusts the closed state to use 'bg-transparent' and 'shodow-none'.

--- frontend/src/components/NavBar.jsx
+++ frontend/src/components/NavBar.jsx
@@ -4,11 +4,7 @@ export default function NavBar() {
   const [open, setOpen] = useState(false);
 
   return (
-    <nav
-      className={`p-5 h-18 md:flex md:items-center md:justify-between fixed w-full endless-river ${
-        open ? "shadow-none" : "shadow-2xl"
-      }`}
-    >
+    <nav className="p-5 h-18 md:flex md:items-center md:justify-between fixed w-full endless-river shadow-2xl">
       <div className="flex justify-between items-center">
         <span className="font-bold text-2xl cursor-pointer">Ofentse.</span>
         <span
@@ -21,8 +17,12 @@ export default function NavBar() {
       <ul
         className={`md:flex md:items-center md:opacity-100 md:static
               absolute left-0 w-full md:w-auto md:py-0 py-4 md:pl-0 pl-7
-              transition-all ease-in duration-500 mt-[-8px] endless-river shadow-2xl
-              ${open ? "top-[80px] opacity-100 " : "top-[-400px] opacity-0"}`}
+              transition-all ease-in duration-500 mt-[-8px]
+              ${
+                open
+                  ? "top-[80px] opacity-100 endless-river shadow-2xl"
+                  : "top-[-400px] opacity-0 bg-transparent shodow-none"
+              }`}
       >
         <li>
           <a href="#about-me" className="text-xl duration-500 mx-4">
