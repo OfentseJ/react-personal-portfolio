efee20f4838bbd2e5efa5c2c98fc1884c7abe50c
Ofentse <ofentsemakhutja@icloud.com>
2025-09-11 12:22:56 +0200

    Update NavBar styles and shadow behavior
    Refactored NavBar to conditionally apply shadow based on menu state and added 'endless-river' class to nav and ul elements. Improved visual consistency and menu transition effects.

--- frontend/src/components/NavBar.jsx
+++ frontend/src/components/NavBar.jsx
@@ -4,7 +4,11 @@ export default function NavBar() {
   const [open, setOpen] = useState(false);
 
   return (
-    <nav className="p-5 h-18 shadow-2xl md:flex md:items-center md:justify-between fixed w-full">
+    <nav
+      className={`p-5 h-18 md:flex md:items-center md:justify-between fixed w-full endless-river ${
+        open ? "shadow-none" : "shadow-2xl"
+      }`}
+    >
       <div className="flex justify-between items-center">
         <span className="font-bold text-2xl cursor-pointer">Ofentse.</span>
         <span
@@ -17,8 +21,8 @@ export default function NavBar() {
       <ul
         className={`md:flex md:items-center md:opacity-100 md:static
               absolute left-0 w-full md:w-auto md:py-0 py-4 md:pl-0 pl-7
-              transition-all ease-in duration-500
-              ${open ? "top-[80px] opacity-100" : "top-[-400px] opacity-0"}`}
+              transition-all ease-in duration-500 mt-[-8px] endless-river shadow-2xl
+              ${open ? "top-[80px] opacity-100 " : "top-[-400px] opacity-0"}`}
       >
         <li>
           <a href="#about-me" className="text-xl duration-500 mx-4">
